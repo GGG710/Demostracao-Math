fca3055034edcdf9baa37efea6f8df06d00a0e25
GGG <127780046+GGG710@users.noreply.github.com>
2023-03-21 13:07:02 +0000

    Pending changes exported from your codespace

---
+++ ClasseMath.java
@@ -0,0 +1,15 @@
+public class ClasseMath {
+    public static void main(String[] args) {
+        System.out.println("Eleva 65 ao cubo: "+ Math.pow(65,3));
+        System.out.println("Raís quadradade 225: "+ Math.sqrt(225));
+        System.out.println("Logarítimo de 128:"+ Math.log(130));
+        System.out.println("Valor absoluto: "+ Math.abs(-56));
+        System.out.println("Arredonda pra cima: "+ Math.ceil(6.5));
+        System.out.println("Arredonda pra baixo: "+ Math.floor(2.5));
+        System.out.println("Arredonda: "+ Math.round(7.6));
+        System.out.println("Valor randômico entre 0 e 1: "+ Math.random());
+        System.out.println("Maior valor: "+ Math.max(15, 3));
+        System.out.println("Menor valor: "+ Math.min(15,3)); 
+        
+    }
+}
